014d6475d7acda106a481bd8015df81d68547770
moonstar <moonstar@localhost>
2018-05-06 21:29:46 +0900

    mmmm

---
+++ DMapdJ/src/main/java/com/firemstar/dmapd/DMapdJApplication.java
@@ -0,0 +1,12 @@
+package com.firemstar.dmapd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DMapdJApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DMapdJApplication.class, args);
+	}
+}
